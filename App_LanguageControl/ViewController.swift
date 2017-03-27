77ac61e6c935901b72550bafba52873d290bb4c9
Jorge Moñiz <jorgemoniz@live.com>
2017-03-27 21:00:53 +0200

    Initial Commit

---
+++ App_LanguageControl/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  App_LanguageControl
+//
+//  Created by Jorge Moñiz on 27/3/17.
+//  Copyright © 2017 jorgemoniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var myTextoLBL: UILabel!
+    @IBOutlet weak var myNumeroLBL: UILabel!
+    @IBOutlet weak var myMonedaLBL: UILabel!
+    @IBOutlet weak var myFechaLBL: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
